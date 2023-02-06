d61ae4e16c2e31843126e15cfb502c60a6bbba3f
Bmattholder <bmattholder@gmail.com>
2023-02-05 18:18:51 -0600

    basic register form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Register from './pages/Register';
+import { useState } from 'react';
+import Home from './pages/Home';
 
 function App() {
+  const [state, setState] = useState({
+    name: '',
+  });
+
+  const onStateChange = (newState) => {
+    setState(newState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Register onStateChange={onStateChange} />
+      {state.name !== '' ? <Home state={state} /> : <></>}
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home({state}) {
+	return (
+		<h1> Hello, {state.name}</h1>
+	)
+}
+
+export default Home
