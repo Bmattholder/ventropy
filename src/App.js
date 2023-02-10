9529cde9564f2618fa09222fc99d2c29aee4da34
Matthew Holder <60249613+Bmattholder@users.noreply.github.com>
2023-02-10 14:47:59 -0600

    Revert "add passing of token to access account page"

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Account from './pages/Account';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [state, setState] = useState({
-    name: '',
-    token: '',
-  });
-
-  const onStateChange = (newState) => {
-    setState(newState);
-  };
-
   return (
-    <div className='container'>
-      <Router>
-      <Header />
-        <Routes>
-          <Route path='/' element={<Home state={state} />} />
-          <Route
-            path='/register'
-            element={<Register onStateChange={onStateChange} />}
-          />
-          <Route
-            path='/login'
-            element={<Login onStateChange={onStateChange} />}
-          />
-          <Route
-            path='/account'
-            element={
-                <Account state={state} />
-            }
-          />
-        </Routes>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
